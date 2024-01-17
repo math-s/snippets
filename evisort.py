88382fa1499f80c4631bd6a6f7dd788cd8325884
Matheus Andrade <matheus.andrade@xa.epicgames.com>
2024-01-17 01:46:50 -0300

    [ aulas de hudson ]

--- evisort.py
+++ evisort.py
@@ -1,5 +1,6 @@
 import asyncio
-from decorator import measure_time
+import time
+import threading
 
 
 def getFilenames(count):
@@ -9,7 +10,18 @@ def getFilenames(count):
     return [f"{i}.txt" for i in range(10)]
 
 
-def readlines(filename):
+def readlines_block(filename):
+    """
+    Placeholder for a resource-bound file reader funcion.
+    Returns file contents as a list of file lines.
+    """
+    num_of_lines = 10 + int(filename.split(".")[0])
+    data = sum(i * i for i in range(10**6))
+    res = [f"line - {i}: {data}" for i in range(num_of_lines)]
+    return res
+
+
+async def readlines(filename):
     """
     Placeholder for a resource-bound file reader funcion.
     Returns file contents as a list of file lines.
@@ -19,25 +31,53 @@ def readlines(filename):
     return [f"line - {i}: {data}" for i in range(num_of_lines)]
 
 
-@measure_time
-def count_total_lines(filenames) -> int:
+async def count_total_lines_(filenames) -> int:
     count = 0
     for file in filenames:
-        count += len(readlines(file))
+        count += len(await readlines(file))
     return count
 
 
+async def count_total_lines(filenames) -> int:
+    filelines = await asyncio.gather(*[readlines(file) for file in filenames])
 
-@measure_time
-async def async_count_total_lines(filenames) -> int:
-    count = 0
-    for file in filenames:
-        count += len(await asyncio.to_thread(readlines, file))
-    return count
+    return sum([len(f) for f in filelines])
+
+
+def count_total_lines_threading(filenames):
+    threads = []
+
+    results = {}
+
+    def run(x):
+        results[x] = len(readlines_block(x))
+
+    for f in filenames:
+        threads.append(threading.Thread(target=run, args=(f,)))
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    return sum(results.values())
 
 
 if __name__ == "__main__":
     filenames = getFilenames(10)
 
-    count_total_lines(filenames)
-    async_count_total_lines(filenames)
+    start = time.time()
+    asyncio.run(count_total_lines(filenames))
+    end = time.time()
+    print(f"naive: {end - start}")
+
+    start = time.time()
+    asyncio.run(count_total_lines_(filenames))
+    end = time.time()
+    print(f"async: {end - start}")
+
+    start = time.time()
+    count_total_lines_threading(filenames)
+    end = time.time()
+    print(f"count_total_lines_threading: {end - start}")
